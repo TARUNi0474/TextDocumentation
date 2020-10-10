d2770ea8adc254510692afb7375751f6951d033d
TARUNi0474 <70103387+TARUNi0474@users.noreply.github.com>
2020-10-10 16:24:31 +0530

    Update conf.py

--- conf.py
+++ conf.py
@@ -17,9 +17,9 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'E-commerce project using Java'
-copyright = '2020, Harideep'
-author = 'Harideep'
+project = 'Android Application Development'
+copyright = '2020, Taruni'
+author = 'TAruni'
 
 # The full version, including alpha/beta/rc tags
 release = ''
